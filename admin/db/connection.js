f9c64ed3698a2d8cfa72df0ddae3e97f78e374d8
sunilkumar110059 <sunilkumar110059@gmail.com>
2021-11-23 11:19:08 +0530

    done2

--- admin/db/connection.js
+++ admin/db/connection.js
@@ -2,8 +2,14 @@ import mongoose from "mongoose";
 
 function ConnctionDB() {
 
+    //  mongodb://localhost:27017/mystore
 
-    mongoose.connect("mongodb://localhost:27017/mystore")
+     // mongodb+srv://mystoredb:<password>@cluster0.8k1dm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+    
+      // 
+    
+    
+    mongoose.connect(process.env.MONGO_URL)
         .then(() => {
             console.log("Connection is Fullfill")
         }).catch((e) => {
