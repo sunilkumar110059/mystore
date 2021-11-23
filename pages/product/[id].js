99d841da4237d38b24258b7dafdb8eefd63d25a0
sunilkumar110059 <sunilkumar110059@gmail.com>
2021-11-23 19:21:20 +0530

    done7

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -30,9 +30,18 @@ import LoadingWrap1 from '../../component/reusablecom/LoadingWrap1';
 export async function getServerSideProps({ params: { id } }) {
     const res = await fetch(`${baseUrl}/api/productApi/${id}`);
     const data = await res.json()
+
+    if (!data) {
+        return {
+          redirect: {
+            destination: '/',
+            permanent: false,
+          },
+        }
+      }
+
     return {
-        props: { productDataById: data },
-        revalidate: 10,
+        props: { productDataById: data }
     }
 }
 
